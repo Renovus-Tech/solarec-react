ce449aa8bddf522b2dfee9b53301183936b53e32
nbartesaghi <nbartesaghi24@gmail.com>
2024-03-14 11:47:05 -0300

    Reload page when endpoint returns status 401
    #11

--- src/helpers/DataAPI.js
+++ src/helpers/DataAPI.js
@@ -6,56 +6,59 @@ export async function DataAPI(options = {}) {
 	  endpoint = {},
     method = 'POST',
     body = {}
-	} = options;
+	} = options
 
-   const fetchOptions = {};
+   const fetchOptions = {}
 
-  //  body.location = getCookie('location');
+  //  body.location = getCookie('location')
 
-   const staging = (process.env.REACT_APP_API_URL === '/data');
+   const staging = (process.env.REACT_APP_API_URL === '/data')
 
-  fetchOptions.method = staging ? 'GET' : method;
+  fetchOptions.method = staging ? 'GET' : method
   fetchOptions.headers = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
     'credentials': 'include',
     'Origin': window.location.href,
     'Access-Control-Request-Method' : staging ? 'GET' : method
-  };
-  fetchOptions.credentials = 'include';
-  // fetchOptions.mode = 'no-cors';
+  }
+  fetchOptions.credentials = 'include'
+  // fetchOptions.mode = 'no-cors'
 
 
 // JSESSIONID
 
-  if (body && !staging && method !== 'GET') fetchOptions.body = JSON.stringify(body);
+  if (body && !staging && method !== 'GET') fetchOptions.body = JSON.stringify(body)
 
   console.log(`Calling DataAPI to ${process.env.REACT_APP_API_URL}/${endpoint} with options:`, options)
 
   const APIResponse = await fetch(
 
     `${process.env.REACT_APP_API_URL}/${endpoint}`, fetchOptions).then(function (response) {
-        return response.json();
+        if (response.status === 401) {
+          setCookie('lastTimeStamp', '')
+          setCookie('name', '')
+          window.location.reload()
+        } else {
+          return response.json()
+        }
       }).catch(
         er => {
-
           if (endpoint === 'admin/locations/current') {
-            setCookie('lastTimeStamp', '');
-            setCookie('name', '');
-            window.location.reload();
+            setCookie('lastTimeStamp', '')
+            setCookie('name', '')
+            window.location.reload()
           }
-          
-          console.log('APIError!', er, `${process.env.REACT_APP_API_URL}/${endpoint}`);
-          // alert('APIError');
+          console.log('APIError!', er, `${process.env.REACT_APP_API_URL}/${endpoint}`)
         }
-      );
+      )
 
-    console.log('fetchOptions for', endpoint, fetchOptions);
-    console.log('APIResponse for', endpoint, APIResponse);
+    console.log('fetchOptions for', endpoint, fetchOptions)
+    console.log('APIResponse for', endpoint, APIResponse)
 
     if (APIResponse && APIResponse.chart && APIResponse.chart.resultCode===404) alert(APIResponse.chart.resultText)
 
-    return APIResponse;
+    return APIResponse
 
   }
 
